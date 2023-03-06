8a05338ef90baa6ba92c6455b945cceaf0764dd5
vikifosse <122542704+vikifosse@users.noreply.github.com>
2023-03-06 16:16:51 +0300

    upd js 2

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const form = document.getElementById('main_form');
 const formCount = document.getElementById('count');
 const level = document.getElementById('level');
 const countDay = document.getElementById('count-day').value;
+const popka = document.getElementById('popka');
+let po = document.querySelector('.table');
+let reset = document.getElementById("reset");
+let inputCalc = document.getElementById('input-calc');
 let day = 0;
 let depcount = 0;
 let depositIndex = '';
@@ -10,15 +14,20 @@ let dayRes = 0;
 let mesRes = 0;
 let result = 0;
 let depNum = 2;
-form.addEventListener('submit', (e) => {
+inputCalc.addEventListener('click', (e) => {
     e.preventDefault();
     depcount = formCount.value;
     checker(formCount);
+    inputCalc.setAttribute("disabled", "disabled");
+    reset.style.display = 'inline-block';
 });
 form.addEventListener('change', (e) => {
     depcount = formCount.value;
 })
 
+reset.addEventListener('click', (e) => {
+    closeBtn();
+});
 
 formCount.addEventListener('input', 
     function(e){
@@ -75,11 +84,35 @@ function DepositCalc(invest, depIndex) {
 }
 
 function calcProfit(invest, percent, countDay, lv) {
+    po.style.display = 'block';
     day = +document.getElementById('count-day').value;
     let deposito = +invest;
     for (let i = 0; i < +day; i++) {
         dayRes = (deposito * ((percent * 1) / 100));
         deposito = (deposito + dayRes);
+        Otris(i+1, percent.toFixed(2), dayRes.toFixed(2), deposito.toFixed(2));
     }
     formCount.value = deposito.toFixed(2);
 }
+
+function Otris(day, prec, prof, total) {
+    popka.innerHTML += 
+        `
+        <tr>
+            <td>${day}</td>
+                <td>${prec}<span>%</span></td>
+                <td><span>$</span>${prof}</td>
+                <td><span>$</span>${total}</td>
+            </tr>
+        `
+    ;
+}
+
+function closeBtn() {
+    inputCalc.removeAttribute("disabled");
+    reset.style.display = 'none';
+    formCount.value = 1000;
+    popka.innerHTML = '';
+    po.style.display = 'none';
+    countDay = 150;
+};
