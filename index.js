66226ede9206b2b39e6e4cace54636c7ab9f2f2f
vikifosse <122542704+vikifosse@users.noreply.github.com>
2023-03-23 14:57:12 +0300

    upd next precent gener

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const form = document.getElementById('main_form');
 const formCount = document.getElementById('count');
 const level = document.getElementById('level');
-const countDay = document.getElementById('count-day').value;
+let countDay = document.getElementById('count-day').value;
 const popka = document.getElementById('popka');
 let po = document.querySelector('.table');
 let reset = document.getElementById("reset");
@@ -29,9 +29,9 @@ reset.addEventListener('click', (e) => {
     closeBtn();
 });
 
-formCount.addEventListener('input', 
-    function(e){
-      this.value = this.value.replace(/[^\d.]/g, '');
+formCount.addEventListener('input',
+    function (e) {
+        this.value = this.value.replace(/[^\d.]/g, '');
     }
 )
 
@@ -61,42 +61,67 @@ function calc(invest, count) {
 function DepositCalc(invest, depIndex) {
     switch (depIndex) {
         case 1:
-            calcProfit(invest, 0.8, countDay);
+            calcProfit(invest, 0.8, depIndex);
             break;
         case 2:
-            calcProfit(invest, 0.95, countDay);
+            calcProfit(invest, 0.95, depIndex);
             break;
         case 3:
-            calcProfit(invest, 1.15, countDay);
+            calcProfit(invest, 1.15, depIndex);
             break;
         case 4:
-            calcProfit(invest, 1.5, countDay);
+            calcProfit(invest, 1.5, depIndex);
             break;
         case 5:
-            calcProfit(invest, 1.85, countDay);
+            calcProfit(invest, 1.85, depIndex);
             break;
         case 6:
-            calcProfit(invest, 2.25, countDay);
+            calcProfit(invest, 2.25, depIndex);
             break;
         default:
             break;
     }
 }
-
-function calcProfit(invest, percent, countDay, lv) {
+let depIndexObj = {
+    1: "0.8",
+    2: "0.95",
+    3: "1.15",
+    4: "1.50",
+    5: "1.85",
+    6: "2.25",
+};
+let precentTotal = 0;
+function calcProfit(invest, percent) {
+    precentTotal = percent;
     po.style.display = 'block';
     day = +document.getElementById('count-day').value;
     let deposito = +invest;
     for (let i = 0; i < +day; i++) {
-        dayRes = (deposito * ((percent * 1) / 100));
+        if (deposito >= 3000) {
+            precentTotal = depIndexObj[2];
+            if (deposito >= 10000) {
+                precentTotal = depIndexObj[3];
+                if (deposito >= 30000) {
+                    precentTotal = depIndexObj[4];
+                    if (deposito >= 60000) {
+                        precentTotal = depIndexObj[5];
+                        if (deposito >= 100000) {
+                            precentTotal = depIndexObj[6];
+                        }
+                    }
+                }
+            }
+        }
+
+        dayRes = (deposito * ((precentTotal * 1) / 100));
         deposito = (deposito + dayRes);
-        Otris(i+1, percent.toFixed(2), dayRes.toFixed(2), deposito.toFixed(2));
+        Otris(i + 1, +precentTotal, dayRes.toFixed(2), deposito.toFixed(2));
     }
     formCount.value = deposito.toFixed(2);
 }
 
 function Otris(day, prec, prof, total) {
-    popka.innerHTML += 
+    popka.innerHTML +=
         `
         <tr>
             <td>${day}</td>
@@ -105,7 +130,7 @@ function Otris(day, prec, prof, total) {
                 <td><span>$</span>${total}</td>
             </tr>
         `
-    ;
+        ;
 }
 
 function closeBtn() {
